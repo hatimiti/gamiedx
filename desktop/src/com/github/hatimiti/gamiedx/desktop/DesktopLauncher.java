e9fc9bd02549042ab0f2e5d3885b1dc28f55807c
hatimiti <hatti33@gmail.com>
2021-06-12 18:21:47 +0900

    Init repos.

---
+++ desktop/src/com/github/hatimiti/gamiedx/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.github.hatimiti.gamiedx.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.github.hatimiti.gamiedx.GamieApplication;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GamieApplication(), config);
+	}
+}
