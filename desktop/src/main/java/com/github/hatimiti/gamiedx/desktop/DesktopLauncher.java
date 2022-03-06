7d30c802592965013b306cd6c43a8c6642c1a049
hatimiti <hatti33@gmail.com>
2022-03-06 16:58:17 +0800

    Add log4j2 settings.

--- desktop/src/main/java/com/github/hatimiti/gamiedx/desktop/DesktopLauncher.java
+++ desktop/src/main/java/com/github/hatimiti/gamiedx/desktop/DesktopLauncher.java
@@ -3,10 +3,16 @@ package com.github.hatimiti.gamiedx.desktop;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 import com.github.hatimiti.gamiedx.GamieApplication;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class DesktopLauncher {
+
+	private static final Logger LOGGER = LogManager.getLogger();
+
 	public static void main(String[] arg) {
 		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		LOGGER.info("start the game: {}", DesktopLauncher.class);
 		new Lwjgl3Application(new GamieApplication(), config);
 	}
 }
